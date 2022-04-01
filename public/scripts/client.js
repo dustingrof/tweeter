b2377bfe87ca54065e32421d9c2323d2cfe51632
Dustin Grof <dustingrof@gmail.com>
2022-03-31 19:35:19 -0700

    various css tweaks and responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,34 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const sampleData = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+const escapeFunc = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function (data) {
   const postTimeAgo = timeago.format(data.created_at);
+  const safeText = escapeFunc(data.content.text);
   const $tweetHTML = $(`
     <article class="tweet">
       <header>
@@ -40,7 +21,7 @@ const createTweetElement = function (data) {
         <div class="username">${data.user.handle}</div>
       </header>
       <p>
-        ${data.content.text}
+        ${safeText}
       </p>
       <footer>
         <div class="date-stamp">${postTimeAgo}</div>
@@ -56,8 +37,15 @@ const createTweetElement = function (data) {
 };
 const renderTweets = function (data) {
   for (const tweet of data) {
-    $(".tweet-list").append(createTweetElement(tweet));
+    $(".tweet-list").prepend(createTweetElement(tweet));
   }
+};
+
+const loadTweets = function () {
+  $.ajax("/tweets", { method: "GET" }).then((tweetsJSON) => {
+    // console.log("Success: ", morePostsHtml);
+    $(".tweets-list").prepend(renderTweets(tweetsJSON));
+  });
 };
 
 $(document).ready(function () {
@@ -68,24 +56,29 @@ $(document).ready(function () {
     event.preventDefault();
     const tweetData = $("#tweet-text").val();
     if (tweetData === "" || tweetData === null) {
-      return alert("Tweet is empty!");
+      $("#tweet-errors")
+        .html("Your tweet is empty, please add some text!")
+        .slideDown("slow")
+        .css("display", "block");
     } else if (tweetData.length > 140) {
-      return alert("Tweet limit exceeded!");
+      return $("#tweet-errors")
+        .html("Tweet is too long, please use a maximum of 140 characters.")
+        .slideDown("slow")
+        .css("display", "block");
+    } else {
+      $("#tweet-errors").slideUp("slow");
     }
     const data = $(this).serialize();
     $.post("/tweets", data).then(() => {
-      console.log("post complete");
+      console.log("post complete", this);
+
+      $("#tweet-list").empty();
       // load tweets function
-      // loadTweets();
+      loadTweets();
+      $("#tweet-form").trigger("reset");
     });
   });
 
-  const loadTweets = function () {
-    $.ajax("/tweets", { method: "GET" }).then((tweetsJSON) => {
-      // console.log("Success: ", morePostsHtml);
-      $(".tweets-list").append(renderTweets(tweetsJSON));
-    });
-  };
   loadTweets();
 });
 
